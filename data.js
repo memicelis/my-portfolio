b5604f59c859d874ec6c6b9c1a46ecbc238050fd
Elis Memic <elismemic5@gmail.com>
2023-07-28 15:51:33 +0200

    adjusted popup window

--- data.js
+++ data.js
@@ -1,73 +1,86 @@
 const projects = [
   {
     id: 1,
-    name: 'Multi-Post Stories',
-    img: './images/multi-post-stories.png',
-    technologies: ['CSS', 'HTML', 'Bootstrap', 'Ruby'],
-    description:
+    name: "Multi-Post Stories",
+    img: "./images/multi-post-stories.png",
+    technologies: ["CSS", "HTML", "Bootstrap", "Ruby"],
+    paragraph:
       " A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
-    linkLiveVersion: 'https://github.com/memicelis',
-    linkSourceVersion: 'https://github.com/memicelis',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    linkLiveVersion: "https://github.com/memicelis",
+    linkSourceVersion: "https://github.com/memicelis",
   },
   {
     id: 2,
-    name: 'Profesional Art Printing Data',
-    img: './images/works-card-background.png',
-    technologies: ['HTML', 'Bootstrap', 'Ruby'],
-    description:
+    name: "Profesional Art Printing Data",
+    img: "./images/works-card-background.png",
+    technologies: ["HTML", "Bootstrap", "Ruby"],
+    paragraph:
       " A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
-    linkLiveVersion: 'https://github.com/memicelis',
-    linkSourceVersion: 'https://github.com/memicelis',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    linkLiveVersion: "https://github.com/memicelis",
+    linkSourceVersion: "https://github.com/memicelis",
   },
   {
     id: 3,
-    name: 'Data Dashboard Healthcare',
-    img: './images/works-card-data-dashboard.png',
-    technologies: ['HTML', 'Bootstrap', 'Ruby'],
-    description:
+    name: "Data Dashboard Healthcare",
+    img: "./images/works-card-data-dashboard.png",
+    technologies: ["HTML", "Bootstrap", "Ruby"],
+    paragraph:
       " A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
-    linkLiveVersion: 'https://github.com/memicelis',
-    linkSourceVersion: 'https://github.com/memicelis',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    linkLiveVersion: "https://github.com/memicelis",
+    linkSourceVersion: "https://github.com/memicelis",
   },
   {
     id: 4,
-    name: 'Website Portfolio',
-    img: './images/works-card-website-portfolio.png',
-    technologies: ['HTML', 'Bootstrap', 'Ruby'],
-    description:
+    name: "Website Portfolio",
+    img: "./images/works-card-website-portfolio.png",
+    technologies: ["HTML", "Bootstrap", "Ruby"],
+    paragraph:
       " A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
-    linkLiveVersion: 'https://github.com/memicelis',
-    linkSourceVersion: 'https://github.com/memicelis',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    linkLiveVersion: "https://github.com/memicelis",
+    linkSourceVersion: "https://github.com/memicelis",
   },
   {
     id: 5,
-    name: 'Profesional Art Printing Data',
-    img: './images/works-card-background.png',
-    technologies: ['HTML', 'Bootstrap', 'Ruby'],
-    description:
+    name: "Profesional Art Printing Data",
+    img: "./images/works-card-background.png",
+    technologies: ["HTML", "Bootstrap", "Ruby"],
+    paragraph:
       " A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
-    linkLiveVersion: 'https://github.com/memicelis',
-    linkSourceVersion: 'https://github.com/memicelis',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    linkSourceVersion: "https://github.com/memicelis",
   },
   {
     id: 6,
-    name: 'Data Dashboard Healthcare',
-    img: './images/works-card-data-dashboard.png',
-    technologies: ['HTML', 'Bootstrap', 'Ruby'],
-    description:
+    name: "Data Dashboard Healthcare",
+    img: "./images/works-card-data-dashboard.png",
+    technologies: ["HTML", "Bootstrap", "Ruby"],
+    paragraph:
       " A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
-    linkLiveVersion: 'https://github.com/memicelis',
-    linkSourceVersion: 'https://github.com/memicelis',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    linkLiveVersion: "https://github.com/memicelis",
+    linkSourceVersion: "https://github.com/memicelis",
   },
   {
     id: 7,
-    name: 'Website Portfolio',
-    img: './images/works-card-website-portfolio.png',
-    technologies: ['HTML', 'Bootstrap', 'Ruby'],
-    description:
+    name: "Website Portfolio",
+    img: "./images/works-card-website-portfolio.png",
+    technologies: ["HTML", "Bootstrap", "Ruby"],
+    paragraph:
       " A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
-    linkLiveVersion: 'https://github.com/memicelis',
-    linkSourceVersion: 'https://github.com/memicelis',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    linkLiveVersion: "https://github.com/memicelis",
+    linkSourceVersion: "https://github.com/memicelis",
   },
 ];
 
